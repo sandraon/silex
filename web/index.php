9186f3e0a2b946b97b04d522a57cb6bfb077532c
Sandra Õnne <sandraonne@gmail.com>
2018-09-21 15:20:31 +0300

    Install Doctrine DBAL with Composer

--- web/index.php
+++ web/index.php
@@ -11,6 +11,13 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
 	'twig.path' => __DIR__.'/views',
 ]);
 
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+	'db.options' => [
+		'driver' => 'pdo_sqlite',
+		'path' => __DIR__.'/app.db',
+	],
+]);
+
 $app->get('/bookings/create', function() use ($app) {
 	return $app['twig']->render('base.html.twig');
 });
